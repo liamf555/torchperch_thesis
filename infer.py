8dedf86ba645c3ff5d884f5e59d845cbe91a58dc
Robert Clarke <robert.clarke@bristol.ac.uk>
2018-05-04 11:29:48 +0100

    Add labels for ben

--- infer.py
+++ infer.py
@@ -39,25 +39,33 @@ times = np.linspace(0, len(states)*0.1, len(states))
 
 fig, axs = plt.subplots(3,2)
 
+for ax in axs.flat:
+    ax.grid()
+    ax.set_xlabel('Time (s)')
+
 # Plot theta
 axs[0,0].plot(times,np.rad2deg(states[:,4,:]))
+axs[0,0].set_ylabel('Pitch angle (deg)')
 
 # Plot q
 axs[0,1].plot(times,np.rad2deg(states[:,10,:]))
+axs[0,1].set_ylabel('Pitch rate (deg/s)')
 
 # Plot sweep
 axs[1,0].plot(times,states[:,12,:])
+axs[1,0].set_ylabel('Sweep angle (deg)')
 
 # Plot elev
 axs[1,1].plot(times,states[:,13,:])
+axs[1,1].set_ylabel('Elevator angle (deg)')
 
 # Plot x vs z
 axs[2,0].plot(states[:,0,:],-states[:,2,:])
+axs[2,0].set_xlabel('x (m)')
+axs[2,0].set_ylabel('z (m)')
 
 # Plot airspeed
 axs[2,1].plot(times,states[:,6,:])
-
-for ax in axs.flat:
-    ax.grid()
+axs[2,1].set_ylabel('Airspeed (m/s)')
 
 plt.show()
