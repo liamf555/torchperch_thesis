87376bb151bda11f543c6e17ffdc0f4093787095
Robert Clarke <robert.clarke@bristol.ac.uk>
2018-05-23 15:44:23 +0100

    Update import naming

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,2 +1,2 @@
-from . import sweep_elevator
-from . import sweep_throttle
+from .sweep_elevator import Bixler_SweepElevator as sweep_elevator
+from .sweep_throttle import Bixler_SweepThrottle as sweep_throttle
