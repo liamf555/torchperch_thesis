3149aceae6a53a72b3506adeb0a3cc534c0d199e
Robert Clarke <robert.clarke@bristol.ac.uk>
2018-05-31 12:54:39 +0100

    Add command line opts to infer Plot throttle if needed

--- infer.py
+++ infer.py
@@ -1,26 +1,52 @@
-import torch, sys, numpy as np
+import torch, sys, argparse, numpy as np
 
 import matplotlib.pyplot as plt
 
-from controllers.sweep_elevator import Bixler_SweepElevator
-
+import controllers
 import scenarios
 
-scenario = None
-if hasattr(scenarios,sys.argv[1]):
-    scenario = getattr(scenarios, sys.argv[1])
+def check_controller(controller_name):
+    if hasattr(controllers,controller_name):
+        return getattr(controllers,controller_name)
+    else:
+        msg = "Could not find controller {}".format(controller_name)
+        raise argparse.ArgumentTypeError(msg)
+
+def check_scenario(scenario_name):
+    if hasattr(scenarios,scenario_name):
+        return getattr(scenarios,scenario_name)
+    else:
+        msg = "Could not find scenario {}".format(scenario_name)
+        raise argparse.ArgumentTypeError(msg)
+
+parser = argparse.ArgumentParser(description='Q-Learning inference for UAV manoeuvres in PyTorch')
+parser.add_argument('--controller', type=check_controller, default='sweep_elevator', help='Which controller to use')
+parser.add_argument('--scenario', type=check_scenario, default='perching', help='Which scenario to execute in')
+parser.add_argument('--scenario-opts', nargs=1, type=str, default='', help='Options for the scenario')
+parser.add_argument('networkFile', type=str, help='The file to obatain weights from')
+args = parser.parse_args()
+
+
+controller = args.controller
+scenario = args.scenario
+
+# Scenario name: scenario.__name__.split('.')[-1]
+# Controller name: controller.__module__.split('.')[-1]
+
+scenario_args = None
+if len(args.scenario_opts) is not 0:
+    scenario_args = scenario.parser.parse_args(args.scenario_opts[0].split(' '))
 else:
-    scenario = scenarios.perching
+    scenario_args = scenario.parser.parse_args([])
 
-scenario_opts = scenario.parser.parse_args(['--no-random-start','--height-limit','5'])
-bixler = scenario.wrap_class(Bixler_SweepElevator,scenario_opts)()
+bixler = scenario.wrap_class(controller, scenario_args)()
 
-networkFile = sys.argv[1]
-if len(sys.argv) > 2:
-    networkFile = sys.argv[2]
+#scenario_opts = scenario.parser.parse_args(['--no-random-start','--height-limit','5'])
+
+networkFile = args.networkFile
 
 from network import QNetwork
-model = QNetwork()
+model = QNetwork(scenario.state_dims,scenario.actions)
 model.load_state_dict(torch.load(networkFile))
 
 bixler.reset_scenario()
@@ -29,7 +55,7 @@ state_history = []
 
 while not bixler.is_terminal():
 
-    bixler_state = bixler.get_state()
+    bixler_state = np.concatenate( (bixler.get_state(), np.array(bixler.throttle,ndmin=2) ), axis=0 )
     print( ','.join(map(str,bixler_state[:,0])) )
     state_history.append(bixler_state)
 
@@ -62,9 +88,14 @@ axs[0,1].set_ylabel('Pitch rate (deg/s)')
 axs[1,0].plot(times,states[:,12,:])
 axs[1,0].set_ylabel('Sweep angle (deg)')
 
-# Plot elev
-axs[1,1].plot(times,states[:,13,:])
-axs[1,1].set_ylabel('Elevator angle (deg)')
+if  controller.__module__.split('.')[-1] == 'sweep_throttle':
+    # Plot throttle
+    axs[1,1].plot(times,states[:,15,:])
+    axs[1,1].set_ylabel('Throttle setting (N)')
+else:
+    # Plot elev
+    axs[1,1].plot(times,states[:,13,:])
+    axs[1,1].set_ylabel('Elevator angle (deg)')
 
 # Plot x vs z
 axs[2,0].plot(states[:,0,:],-states[:,2,:])
