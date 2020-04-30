063d43a8152d1c032a6ab16e01f1bc7556eae442
Liam Fletcher <liam.fletcher@bristol.ac.uk>
2020-04-30 12:39:22 +0100

    new reward test

--- gym_learn.py
+++ gym_learn.py
@@ -73,7 +73,7 @@ with open(args.param_file) as json_file:
 log_dir = params.get("log_file")
 
 wandb.config.update(params)
-wandb.config.timesteps=5000
+wandb.config.timesteps=5000000
 
 # env = gym.make(params.get("env"), parameters=params)
 
