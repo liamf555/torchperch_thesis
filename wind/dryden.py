9c3673009e6953026eb584b4ff69dda2dede68b7
Liam Fletcher <liam.fletcher@bristol.ac.uk>
2021-02-18 14:25:01 +0000

    refined dryden

--- wind/dryden.py
+++ wind/dryden.py
@@ -1,49 +1,5 @@
-import math
+
 import numpy as np
-from scipy import signal
-import wandb
-
-from scipy.signal import lti, TransferFunction
-from math import sqrt
-import matplotlib.pyplot as plt
-
-class Filter:
-    def __init__(self, num, den):
-        """
-        Wrapper for the scipy LTI system class.
-        :param num: numerator of transfer function
-        :param den: denominator of transfer function
-        """
-        self.filter = lti(num, den)
-        self.x = None
-        self.y = None
-        self.t = None
-
-    def simulate(self, u, t):
-        """
-        Simulate filter
-        :param u: filter input
-        :param t: time steps for which to simulate
-        :return: filter output
-        """
-        if self.x is None:
-            x_0 = None
-        else:
-            x_0 = self.x[-1]
-
-        self.t, self.y, self.x = self.filter.output(U=u, T=t, X0=x_0)
-
-        return self.y
-
-    def reset(self):
-        """
-        Reset filter
-        :return:
-        """
-        self.x = None
-        self.y = None
-        self.t = None
-
 
 class DrydenGustModel:
     def __init__(self, dt = 0.1, b=1.5, Va=13, intensity='light'):
@@ -69,54 +25,25 @@ class DrydenGustModel:
             self.sigma_w =  1.4
 
         # Turbulence length scales
-        self.L_u = 200
-        self.L_v = self.L_u
-        self.L_w = 50
+        self.Lu = 200
+        self.Lv = self.Lu
+        self.Lw = 50
 
-        # K_u = self.sigma_u * math.sqrt((2 * self.L_u) / (math.pi * self.Va))
-        # K_v = self.sigma_v * math.sqrt((self.L_v) / (math.pi * self.Va))
-        # K_w = self.sigma_w * math.sqrt((self.L_w) / (math.pi * self.Va))
-
-        # T_u = self.L_u / self.Va
-        # T_v1 = math.sqrt(3.0) * self.L_v / self.Va
-        # T_v2 = self.L_v / self.Va
-        # T_w1 = math.sqrt(3.0) * self.L_w / self.Va
-        # T_w2 = self.L_w / self.Va
-
-        # K_p = self.sigma_w * math.sqrt(0.8 / self.Va) * ((math.pi / (4 * b)) ** (1 / 6)) / ((self.L_w) ** (1 / 3))
-        # K_q = 1 / self.Va
-        # K_r = K_q
-
-        # T_p = 4 * b / (math.pi * self.Va)
-        # T_q = T_p
-        # T_r = 3 * b / (math.pi * self.Va)
-
-        # # self.filters = {"H_u": Filter(K_u, [T_u, 1]),
-        # #                 "H_w": Filter([K_w * T_w1, K_w], [T_w2 ** 2, 2 * T_w2, 1]),}
-        # self.filters = {"H_u": Filter(K_u, [T_u, 1]),
-        #                 "H_v": Filter([ K_v * T_v1,K_v], [T_v2 ** 2, 2 * T_v2, 1]),
-        #                 "H_w": Filter([K_w * T_w1, K_w], [T_w2 ** 2, 2 * T_w2, 1]),
-        #                 "H_p": Filter(K_p, [T_p, 1]),
-        #                 "H_q": Filter([-K_w * K_q * T_w1, -K_w * K_q, 0], [T_q * T_w2 ** 2, T_w2 ** 2 + 2 * T_q * T_w2, T_q + 2 * T_w2, 1]),
-        #                 "H_r": Filter([K_v * K_r * T_v1, K_v * K_r, 0], [T_r * T_v2 ** 2, T_v2 ** 2 + 2 * T_r * T_v2, T_r + 2 * T_v2, 1]),}
-
-        self.sim_length = 0
-        self.dt = dt
-
-        self.noise = None
-
-        self.vel_lin = None
-        self.vel_ang = None
-
-        self._gust_u = 0.0
-        # self._gust_v = 0.0
-        self._gust_w = 0.0
-        # self._gust_q = 0.0
-
-        self.gust_u = 0.0
-        # self.gust_v = 0.0
-        self.gust_w = 0.0
-        # self.gust_q = 0.0
+        self._A = np.array([[-Va/self.Lu, 0, 0, 0, 0],
+                            [0, -2*(Va/self.Lv), -(Va/self.Lv)**2, 0, 0],
+                            [0, 1, 0, 0, 0],
+                            [0, 0, 0, -2*(Va/self.Lw), -(Va/self.Lw)**2],
+                            [0, 0, 0, 1, 0]])
+        self._B = np.array([[1, 0, 0],
+                            [0, 1, 0],
+                            [0, 0, 0],
+                            [0, 0, 1],
+                            [0, 0, 0]])
+        self._C = np.array([[self.sigma_u * np.sqrt((2*Va)/(np.pi * self.Lu)), 0, 0, 0, 0],
+                            [0, self.sigma_v * np.sqrt((3*Va)/(np.pi * self.Lv)), np.sqrt((Va/self.Lv)**3), 0, 0],
+                            [0, 0, 0, self.sigma_w * np.sqrt((3*Va)/(np.pi* self.Lv)), np.sqrt((Va/self.Lw)**3)]])
+        self._gust_state = np.zeros((5, 1))
+        self.gust = np.zeros((1, 3))
 
     def seed(self, seed=None):
         """
@@ -126,155 +53,37 @@ class DrydenGustModel:
         """
         self.np_random = np.random.RandomState(seed)
 
-    def _generate_noise(self, size):
-        self.noise = np.sqrt(np.pi / self.dt) * self.np_random.standard_normal(size=(4, size)) 
-
-        # print(self.noise)
-        return self.noise
-
-    # def reset(self, noise=None):
-    #     """
-    #     Reset model.
-    #     :param noise: (np.array) Input to filters, should be four sequences of Gaussianly distributed numbers.
-    #     :return:
-    #     """
-    #     self.vel_lin = None
-    #     # self.vel_ang = None
-    #     self.sim_length = 0
-
-    #     if noise is not None:
-    #         assert len(noise.shape) == 2
-    #         assert noise.shape[0] == 4
-    #         noise = noise * math.sqrt(math.pi / self.dt)
-    #     self.noise = noise
-
-    #     for filter in self.filters.values():
-    #         filter.reset()
 
     def reset(self):
-        self._gust_u = 0.0
-        self._gust_w = 0.0
-        self.gust = []
-
-    def simulate(self, length):
-        """
-        Simulate turbulence by passing band-limited Gaussian noise of length length through the shaping filters.
-        :param length: (int) the number of steps to simulate.
-        :return:
-        """
-        t_span = [self.sim_length, self.sim_length + length]
-
-        # t = np.linspace(t_span[0] * self.dt, t_span[1] * self.dt, length)
-        t = np.linspace(t_span[0], t_span[1], (t_span[1] - t_span[0]) / self.dt)
-
-        # print(len(t))
-
-        if self.noise is None:
-            noise = self._generate_noise(t.shape[0])
-        else:
-            if self.noise.shape[-1] >= t_span[1]:
-                noise = self.noise[:, t_span[0]:t_span[1]]
-            else:
-                noise_start_i = t_span[0] % self.noise.shape[-1]
-                remaining_noise_length = self.noise.shape[-1] - noise_start_i
-                if remaining_noise_length >= length:
-                    noise = self.noise[:, noise_start_i:noise_start_i + length]
-                else:
-                    if length - remaining_noise_length > self.noise.shape[-1]:
-                        concat_noise = np.pad(self.noise,
-                                              ((0, 0), (0, length - remaining_noise_length - self.noise.shape[-1])),
-                                              mode="wrap")
-                    else:
-                        concat_noise = self.noise[:, :length - remaining_noise_length]
-                    noise = np.concatenate((self.noise[:, noise_start_i:], concat_noise), axis=-1)
-
-
-        vel_lin = np.array([self.filters["H_u"].simulate(noise[0], t),
-                            self.filters["H_v"].simulate(noise[1], t),
-                            self.filters["H_w"].simulate(noise[2], t)])
-
-        vel_ang = np.array([self.filters["H_p"].simulate(noise[3], t),
-                            self.filters["H_q"].simulate(noise[1], t),
-                            self.filters["H_r"].simulate(noise[2], t)])
-
-        if self.vel_lin is None:
-            self.vel_lin = vel_lin
-            self.vel_ang = vel_ang
-        else:
-            self.vel_lin = np.concatenate((self.vel_lin, vel_lin), axis=1)
-            self.vel_ang = np.concatenate((self.vel_ang, vel_ang), axis=1)
-
-        self.sim_length += length
-
-    def _euler(self, tfs):
-
-        self.ss_u = tfs["H_u"].to_ss()
-        # self.ss_v = self.filters["H_v"].filter.to_ss()
-        self.ss_w = tfs["H_w"].to_ss()
-        # self.ss_q = self.filters["H_q"].filter.to_ss()
-
-    def _gust(self, dt):
-
-        # print(dt)
-        noise = np.sqrt(np.pi / dt) * self.np_random.standard_normal(2)
-
-        # print(noise)
-      
-        # propagate Dryden model (Euler method): x[k+1] = x[k] + Ts*( A x[k] + B w[k] )
-        #self._gust_state += self._Ts * (self._A @ self._gust_state + self._B * w)
-        self._gust_u += dt * (self.ss_u.A * self._gust_u + self.ss_u.B * noise[0])
-        gust_u = self.ss_u.C @ self._gust_u
-
-        # self._gust_v += self.dt * (self.ss_v.A * self._gust_v + self.ss_v.B * noise[1])
-        # gust_v = self.ss_v.C @ self._gust_v
-
-        self._gust_w += dt * (self.ss_w.A * self._gust_w + self.ss_w.B * noise[1])
-        gust_w = self.ss_w.C @ self._gust_w
-
-        # self._gust_q += self.dt * (self.ss_q.A * self._gust_q + self.ss_q.B * noise[1])
-        # gust_q = self.ss_q.C @ self._gust_q
-
-        # self.gust = np.array([[gust_u.item(0),gust_v.item(0),gust_w.item(0), gust_q.item(0)]]).T
-
-        # print(gust_u.item(0))
-
-        # wandb.log({'dryden_u': gust_u.item(0)})
-
-        self.gust = np.array([[gust_u.item(0), 0.0,  gust_w.item(0)]])
-        # print(self.gust)
-
-        # self.gust = np.array([[0.0, 0.0,  0.0]])
-
-        return self.gust
+        self._gust_state = np.zeros((5, 1))
 
     def update(self, Va, dt):
+        self._A = np.array([[-Va/self.Lu, 0, 0, 0, 0],
+                            [0, -2*(Va/self.Lv), -(Va/self.Lv)**2, 0, 0],
+                            [0, 1, 0, 0, 0],
+                            [0, 0, 0, -2*(Va/self.Lw), -(Va/self.Lw)**2],
+                            [0, 0, 0, 1, 0]])
+        self._C = np.array([[self.sigma_u * np.sqrt((2*Va)/(np.pi * self.Lu)), 0, 0, 0, 0],
+                            [0, self.sigma_v * np.sqrt((3*Va)/(np.pi  * self.Lv)), np.sqrt((Va/self.Lv)**3), 0, 0],
+                            [0, 0, 0, self.sigma_w * np.sqrt((3*Va)/(np.pi * self.Lv)), np.sqrt((Va/self.Lw)**3)]])
 
-        if self.intensity == 'none':
-            return np.array([[0.0, 0.0, 0.0]])
+        # calculate wind gust using Dryden model.  Gust is defined in the body frame
+        w = np.sqrt(np.pi / dt) * self.np_random.standard_normal(3)
+        w = [[x] for x in w]
+        # propagate Dryden model (Euler method): x[k+1] = x[k] + Ts*( A x[k] + B w[k] )
+        self._gust_state += dt * (self._A @ self._gust_state + self._B @ w)
 
-        # print(Va)
-        K_u = self.sigma_u * math.sqrt((2 * self.L_u) / (math.pi * Va))
-        # K_v = sigma_v * math.sqrt((L_v) / (math.pi * self.Va))
-        K_w = self.sigma_w * math.sqrt((self.L_w) / (math.pi * Va))
+        _gusts = self._C @ self._gust_state
 
-        T_u = self.L_u / Va
-        # T_v1 = math.sqrt(3.0) * L_v / self.Va
-        # T_v2 = L_v / self.Va
-        T_w1 = math.sqrt(3.0) * self.L_w / Va
-        T_w2 = self.L_w / Va
+        self.gust = np.array([[_gusts.item(0), 0.0, _gusts.item(2)]])
 
-        tfs = {"H_u": TransferFunction(K_u, [T_u, 1]),
-                        "H_w": TransferFunction([K_w * T_w1, K_w], [T_w2 ** 2, 2 * T_w2, 1]),}
-
-        self._euler(tfs)
-
-        return self._gust(dt)
+        return self.gust
 
 if __name__ == "__main__":
 
     dryden = DrydenGustModel()
 
-    print('goat')
+
 
     # dryden.euler()
 
